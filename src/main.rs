04454d3b649dc9f6ac3e6666c5970cf8dd58baed
Flix <f.dommes@famedly.com>
2022-04-19 23:33:48 +0200

    refactor: Split apart frame processor and frame retriever

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,11 @@ fn main() -> Result<()> {
 	let event_sender = launcher.get_external_handle();
 
 	let camera_handler = CameraConnector::new(event_sender, pick_receiver);
-	let cam_handle = camera_handler.spawn();
+	let cam_handles = camera_handler.spawn();
 
 	launcher.log_to_console().launch(RootUIState::default()).expect("running app");
 
-	cam_handle.join().expect("joining camera handler");
+	cam_handles.0.join().expect("joining camera frame receiver");
+	cam_handles.1.join().expect("joining camera frame processor");
 	Ok(())
 }
